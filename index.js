2004f1080dbe25746fa0edf5d5c358e36abc55ab
Wilbert Verayin <wilbertverayin@gmail.com>
2024-08-05 18:51:47 +0800

    chore: remove overrides

--- index.js
+++ index.js
@@ -188,21 +188,4 @@ module.exports = {
             },
         ],
     },
-
-    overrides: [
-        {
-            files: [ '*.vue' ],
-            rules: {
-                indent: 'off',
-            },
-        },
-        {
-            files: [ '**/*.cy.{js,jsx,ts,tsx}' ],
-            extends: [
-                // Add Cypress-specific lint rules, globals and Cypress plugin
-                // See https://github.com/cypress-io/eslint-plugin-cypress#rules
-                'plugin:cypress/recommended',
-            ],
-        },
-    ],
 };
