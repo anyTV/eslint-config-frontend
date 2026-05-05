b55912bd2c2da04903eaa62ee95e742a94a4353e
Sherwin Ferrer <sherwin.ferrer@freedom.tm>
2026-05-05 17:58:40 +0800

    fix: remove unused imports

--- index.mjs
+++ index.mjs
@@ -4,13 +4,8 @@ import tseslint from "typescript-eslint";
 import { fixupPluginRules } from "@eslint/compat";
 import globals from "globals";
 import parser from "vue-eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import stylistic from '@stylistic/eslint-plugin';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default tseslint.config(
     js.configs.recommended,
